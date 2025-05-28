89268b3e88c6de3c2f268a229aaa551fa9ccd87d
TiffanyYang411 <suhanyang902@gmail.com>
2025-05-28 16:59:10 +0800

    spa-0528-17:00

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './styles/typography-variables.css'; // ✅ 放這裡，全站所有元件都吃得到
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
