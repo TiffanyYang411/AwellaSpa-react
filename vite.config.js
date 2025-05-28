9a0a44eb46588d39806d4d44dc08621bd77752c9
TiffanyYang411 <suhanyang902@gmail.com>
2025-05-28 14:40:59 +0800

    spa-0528-14:40

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AwellaSpa-react/', // ✅ 加上這一行（根據你的 repo 名稱）
   plugins: [react()],
 })
